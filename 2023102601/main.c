3054c41fb4852923a066e5dcc67b07ebae343e22
Thalineguedes <111344896+Thalineguedes@users.noreply.github.com>
2023-11-15 20:18:37 -0300

    Update main.c

--- 2023102601/main.c
+++ 2023102601/main.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct{
+char nome [50];
+char nascimento [11];
+char CPF [12]; 
 
+}Pessoa;
+int Preencherdados (Pessoa* pessoa){
+  printf("Digite o nome: ");
+  scanf("%s", pessoa->nome);
+  printf("Digite a data de nascimento: ");
+  scanf("%s", pessoa->nascimento);
+  printf("Digite o CPF: ");
+  scanf("%s", pessoa->CPF);
+}
+  int imprimirdados (Pessoa*pessoa){
+    printf("Nome: %s\n", pessoa->nome);
+    printf("Data de nascimento: %s\n", pessoa->nascimento);
+    printf("CPF: %s\n", pessoa->CPF);
+  }
+int main() {
+  Pessoa* pessoa = (Pessoa*) malloc(sizeof(Pessoa));
+  Preencherdados(pessoa);
+  imprimirdados(pessoa);
+  free(pessoa);
+
+  return 0;
+}
