e320bda98834cece6f9dcaf3e8aef1cfc1869042
Thalineguedes <111344896+Thalineguedes@users.noreply.github.com>
2023-11-15 20:20:19 -0300

    Update main.c

--- 2023102603/main.c
+++ 2023102603/main.c
@@ -1,1 +1,29 @@
+include <stdio.h>
+#include <stdlib.h>
+typedef struct{
+ float lado;
+}Cubo;
+int iniciarCubo(Cubo* cubo, float lado){
+  cubo->lado = lado;
+}
+float calcularArea (Cubo* cubo){
+  return 6 * cubo->lado * cubo->lado;
+}
+float calcularVolume(Cubo* cubo){
+  return cubo->lado * cubo->lado * cubo->lado;
+}
+float obterLado(Cubo* cubo){
+  return cubo->lado;
+}
+int main(){
+  Cubo cubo;
+  float lado;
+  printf("Digite o tamanho do lado do cubo: ");
+  scanf("%f", &lado);
+  iniciarCubo(&cubo, lado);
+  printf("lado do cubo: %.2f\n", obterLado(&cubo));
+  printf("Àrea do cubo: %.2f\n", calcularArea(&cubo));
+  printf("Volume ddo cubo: %.2f\n", calcularVolume(&cubo));
 
+  return 0;
+}
