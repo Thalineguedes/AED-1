628415715828bdb0952cbbd602307ce005e9a994
Thalineguedes <111344896+Thalineguedes@users.noreply.github.com>
2023-11-15 20:08:14 -0300

    Update ponto.h

--- 2023102600 /ponto.h
+++ 2023102600 /ponto.h
@@ -1,1 +1,16 @@
+#ifndef PONTO_H_INCLUDED
+#define PONTO_H_INCLUDED
 
+struct ponto {
+  float x;
+  float y;
+};
+
+typedef struct ponto Ponto;
+Ponto* Cria_ponto (float x, float y);
+void libera_ponto (Ponto *pto);
+int acessa_ponto (Ponto *pto, float *x, float *y);
+int atribuir_ponto (Ponto *pto, float x, float y);
+float distancia_ponto (Ponto *pt01, Ponto *pto2);
+
+#endif // PONTO_HINCLUDED 
